bf727430f7c47b6d1b726ab0a82d297c2a615f9e
Tom Davis <tom@tomdavislx.co.uk>
2018-06-06 22:14:38 +0100

    Initial Commit

---
+++ ChatOSC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChatOSC
+//
+//  Created by Tom Davis on 06/06/2018.
+//  Copyright © 2018 Tom Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
